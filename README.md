f527100ba7801d54396cb324c5284f19f71fa0f7
Nan Wang <nan.wang@jina.ai>
2021-09-27 00:04:25 +0800

    chore: clean up

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # example-video-search
 This is an example of search videos using jina
+
+```bash
+python app.py -m restful
+```
+
+## How it works
+When sending requests to `/index`, the video files in the will be processed as below 
+1. The frames are extracted from the videos and saved in the chunks
+2. Each frame in the chunks are encoded into vectors by `CLIPImageEncoder`
+3. All the frame are indexed by `SimpleIndexer`
+
+After indexing, the search requests are sent to `/search` with the query in text. The query text are processed
+1. The text is encoded into vectors by `CLIPTextEncoder`
+2. The encoded vectors are used to retrieve the top K nearest neighbors (frames) from the index
+3. The retrieved frames are aggregated to find the most related videos. The timestamp of the most related frame in the videos is stored in `Document.tags['timestamp']`
--- app.py
+++ app.py
@@ -9,25 +9,20 @@ from jina.types.request import Request
 def config():
     os.environ['JINA_PORT'] = '45678'
     os.environ['JINA_WORKSPACE'] = './workspace'
-
+    os.environ['TOP_K'] = '50'
 
 def get_docs(data_path):
     for fn in glob.glob(os.path.join(data_path, '*.mp4')):
         yield Document(uri=fn, id=os.path.basename(fn))
 
 
-def check_index(resp: Request):
-    for doc in resp.docs:
-        print(f'{doc.uri}')
-        for c in doc.chunks:
-            print(f'+- {c.uri}, {c.embedding.shape}')
-
-
 def check_search(resp: Request):
     for doc in resp.docs:
-        print(f'{doc.uri}, {doc.text}')
+        print(f'Query text: {doc.text}')
+        print(f'Matches:')
         for m in doc.matches:
-            print(f'+- {m.id}, {m.location}, {m.scores["cosine"].value}, {m.tags["timestamp"]}')
+            print(f'+- id: {m.id}, score: {m.scores["cosine"].value}, timestampe: {m.tags["timestamp"]}')
+        print('-'*10)
 
 
 @click.command()
@@ -50,18 +45,24 @@ def main(mode):
             override_with={
                 'protocol': 'grpc',
                 'cors': False})
+    elif mode == 'restful':
+        f = Flow.load_config('flow.yml')
     with f:
         f.post(
             on='/index',
-            inputs=get_docs('toy_data'),
-            on_done=check_index)
+            inputs=get_docs('toy_data'))
+        if mode == 'grpc':
+            f.post(
+                on='/search',
+                inputs=DocumentArray([
+                    Document(text='mountain and road'),
+                    Document(text='This is a world map'),
+                    Document(text='There are many flags of different countries'),
+                ]),
+                on_done=check_search)
+        elif mode == 'restful':
+            f.block()
 
-        f.post(
-            on='/search',
-            inputs=DocumentArray([
-                Document(text='mountain and road'),
-                Document(text='world map')]),
-            on_done=check_search)
 
 if __name__ == '__main__':
     main()
